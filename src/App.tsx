71f876d9a64f02253748c91bf42522a3b3617eda
Lucas Ezidro <you@example.com>
2021-07-03 20:03:23 -0300

    first day project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Home } from './pages/Home'
+import '../src/styles/Global.css'
+
+
+function App() {
+
+
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,62 @@
+import logo from '../assets/images/logo.jpeg'
+import avatar from '../assets/images/avatar.jpg'
+import martelo from '../assets/images/martelo.png'
+import trabalhadores from '../assets/images/trabalhadores.png'
+import pincel from '../assets/images/pincel.png'
+import '../styles/Home.css'
+
+export function Home(){
+    return(
+
+        <div id="container">
+            <header>
+                <img src={logo} alt="Logo Ezidros" className="logo" />
+
+                <h1 className="title">
+                    Ezidros Enterprise
+                </h1>
+
+                <span className="span-header">Serviços de confiança</span>
+                <a href="/saibaMais">
+                    Saiba mais
+                </a>
+
+                <button 
+                    type="button" 
+                    className="btn-header"
+                    
+                    >
+                        Fazer Um Orçamento
+                </button>
+            </header>
+
+            <main className="body">
+                <img src={avatar} alt="avatar" className="avatar"/>
+
+                <div className="card-1">
+                    <img src={martelo} alt="logo martelo" className="martelo" />
+
+                    <span className="span-card1">
+                        Serviços de pequeno/médio porte de manunteções gerais.
+                    </span>
+                </div>
+
+                <div className="card-2">
+                    <img src={trabalhadores} alt="logo martelo" className="martelo" />
+
+                    <span className="span-card2">
+                        Trabalhadores eficazes e prontos para lhe atender.
+                    </span>
+                </div>
+
+                <div className="card-3">
+                    <img src={pincel} alt="logo martelo" className="martelo" />
+
+                    <span className="span-card3">
+                        Serviços de pinturas residenciais.
+                    </span>
+                </div>
+            </main>
+        </div>
+    )
+}
